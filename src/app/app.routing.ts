369776b1820f7bdeb099313c9c959348d3dfb273
Baha El Kassaby <Baha.ElKassaby@jax.org>
2022-01-21 15:27:56 -0500

    Remove unnecessary AdminLayout and put main modules in appComponent

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,32 +3,29 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { SnpsIndelsComponent } from './analysis/snps-indels/snps-indels.component';
+import { StrainVariantComponent } from './analysis/strain-variant/strain-variant.component';
+import { FilesNavComponent } from './files-nav/files-nav.component';
+import { MvarApiComponent } from './mvar-api';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 const routes: Routes =[
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
-    path: '',
-    component: AdminLayoutComponent,
-    children: [{
-      path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-    }]
-  }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'notifications', component: NotificationsComponent },
+  { path: 'variant', component: SnpsIndelsComponent },
+  { path: 'strain-variant', component: StrainVariantComponent },
+  { path: 'filesNav', component: FilesNavComponent },
+  { path: 'mvar-api', component: MvarApiComponent },
+  { path: 'about', component: AboutComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' },
 ];
 
-@NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
-    })
-  ],
-  exports: [
-  ],
-})
-export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(routes);
+
