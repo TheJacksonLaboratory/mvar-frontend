54615356c28dd85b47e0e3d8691edeec5a0107f7
waltob <benjamin.walton@jax.org>
2025-04-14 09:44:10 -0400

    add Google Analytics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -27,5 +27,6 @@ export const environment = {
     SEQUENCE_ONTOLOGY_URL: 'http://www.sequenceontology.org/browser/current_release/term/',
     JAX_STRAIN_REGISTRY_URL: 'https://www.jax.org/strain/',
     JAX_MAMMALIAN_PHENOTYPE_URL: 'http://www.informatics.jax.org/vocab/mp_ontology/',
-    production: true
+    production: true,
+    cookieDomain: 'jax.org'
 };
